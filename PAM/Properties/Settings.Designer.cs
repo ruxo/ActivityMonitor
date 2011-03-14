1f5156accf333975e487289bd94b9d629551135f
4rchie <devnull@localhost>
2011-03-14 16:19:54 +0100

    fixing settings

--- PAM/Properties/Settings.Designer.cs
+++ PAM/Properties/Settings.Designer.cs
@@ -8,18 +8,37 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace PAM.Properties {
-    
-    
+namespace PAM.Properties
+{
+
+
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "10.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
-        
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
+    {
+
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-        
-        public static Settings Default {
-            get {
+
+        public static Settings Default
+        {
+            get
+            {
                 return defaultInstance;
+            }
+        }
+
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("30")]
+        public int IdleTime
+        {
+            get
+            {
+                return ((int)(this["IdleTime"]));
+            }
+            set
+            {
+                this["IdleTime"] = value;
             }
         }
     }
